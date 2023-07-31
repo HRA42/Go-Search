13da9fc7a87412f2dbd46cc139a91f614d159e06
Henry Rausch <44985923+HRA42@users.noreply.github.com>
2023-07-31 09:28:44 +0000

    feat(randomarray, sort): implement functions to create random integer arrays and sort arrays

---
+++ sortarray/sort.go
@@ -0,0 +1,37 @@
+package sortarray
+
+// Implements Merge Sort in case the data must be sorted (e.g. bin search)
+func Sort(arr []int) []int {
+	if len(arr) <= 1 {
+		return arr
+	}
+
+	mid := len(arr) / 2
+
+	left := Sort(arr[:mid])
+	right := Sort(arr[mid:])
+
+	return merge(left, right)
+}
+
+func merge(left, right []int) []int {
+	size, i, j := len(left)+len(right), 0, 0
+	result := make([]int, size)
+
+	for k := 0; k < size; k++ {
+		if i > len(left)-1 && j <= len(right)-1 {
+			result[k] = right[j]
+			j++
+		} else if j > len(right)-1 && i <= len(left)-1 {
+			result[k] = left[i]
+			i++
+		} else if left[i] < right[j] {
+			result[k] = left[i]
+			i++
+		} else {
+			result[k] = right[j]
+			j++
+		}
+	}
+	return result
+}
