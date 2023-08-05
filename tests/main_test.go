2c72d030eebe3b3d0cc5d12ca985b627fef634af
Henry Rausch <44985923+HRA42@users.noreply.github.com>
2023-08-05 18:44:06 +0200

    Extend main_test to include benchmarks for search algorithms
    Introduced benchmark tests for both linear and binary search functions, examining their respective performance with input sizes ranging from 10 to 100,000,000. This will provide a quantitative comparison between the two, giving insight into their efficiency under different scenarios. Also, renamed 'data' to 'testData' and 'target' to 'testTarget' to enhance code readability.

--- tests/main_test.go
+++ tests/main_test.go
@@ -1,26 +1,74 @@
 package main
 
 import (
+	"fmt"
 	"github.com/hra42/go-search/internal/binarysearch"
 	"github.com/hra42/go-search/internal/linearsearch"
+	"github.com/hra42/go-search/internal/randomarray"
 	"github.com/hra42/go-search/internal/sortarray"
+	"math"
 	"testing"
+	"time"
 )
 
-var data = []int{5, 8, 9, 2, 3, 7, 4, 1, 6}
-var target = 7
+var testData = []int{5, 8, 9, 2, 3, 7, 4, 1, 6}
+var testTarget = 7
+
+var benchmarkCases = []struct {
+	input int
+}{
+	{input: 10},
+	{input: 100},
+	{input: 1000},
+	{input: 10000},
+	{input: 100000},
+	{input: 1000000},
+	{input: 10000000},
+	{input: 100000000},
+}
 
 func TestLinearSearch(t *testing.T) {
-	result := linearsearch.Search(data, target)
+	result := linearsearch.Search(testData, testTarget)
 	if !result {
-		t.Errorf("Expected to find the target %v, but it was not found", target)
+		t.Errorf("Expected to find the target %v, but it was not found", testTarget)
 	}
 }
 
 func TestBinarySearch(t *testing.T) {
-	data = sortarray.Sort(data)
-	result := binarysearch.Search(data, target)
+	testData = sortarray.Sort(testData)
+	result := binarysearch.Search(testData, testTarget)
 	if !result {
-		t.Errorf("Expected to find the target %v, but it was not found", target)
+		t.Errorf("Expected to find the target %v, but it was not found", testTarget)
+	}
+}
+
+func BenchmarkLinearSearch(b *testing.B) {
+	for _, v := range benchmarkCases {
+		b.Run(fmt.Sprintf("input_size_%d", v.input), func(b *testing.B) {
+			var duration time.Duration
+			for i := 0; i < b.N; i++ {
+				start := time.Now()
+				arr := randomarray.CreateRandomIntArray(v.input, math.MaxInt)
+				linearsearch.Search(arr, testTarget)
+				duration = time.Since(start)
+			}
+			fmt.Printf("Für die Eingabe: %v wurde %v benötigt\n", v.input, duration)
+		})
+	}
+}
+
+func BenchmarkBinarySearch(b *testing.B) {
+	for _, v := range benchmarkCases {
+		b.Run(fmt.Sprintf("input_size_%d", v.input), func(b *testing.B) {
+			var duration time.Duration
+			for i := 0; i < b.N; i++ {
+				start := time.Now()
+				arr := randomarray.CreateRandomIntArray(v.input, math.MaxInt)
+				arr = sortarray.Sort(arr)
+				binarysearch.Search(arr, testTarget)
+				duration = time.Since(start)
+			}
+			fmt.Printf("Für die Eingabe: %v wurde %v benötigt\n", v.input, duration)
+		})
 	}
 }
